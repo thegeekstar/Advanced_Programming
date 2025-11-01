4bd6ee0ea3f0add6a67a43bd57a2ee9f20bc084d
Amirreza Soltani <soltaniamirreza3@gmail.com>
2025-11-01 22:09:32 +0330

    E_6_18: Announced

---
+++ Exercises/src/exercise6_18.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class exercise6_18 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter side length: ");
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
